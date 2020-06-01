c0b36b17018edf093969776d3cdfaf20c0e515b1
vtal <vtal@vtal>
2020-06-01 16:01:09 +0300

    starting to embrace gstreamer for nano

--- realfun/facerec.py
+++ realfun/facerec.py
@@ -5,7 +5,8 @@ import numpy as np
 
 HAAR_CASCADE_XML_FILE_FACE = "/usr/share/opencv4/haarcascades/haarcascade_frontalface_default.xml"
 
-GSTREAMER_PIPELINE = 'nvarguscamerasrc ! video/x-raw(memory:NVMM), width=3280, height=2464, format=(string)NV12, framerate=21/1 ! nvvidconv flip-method=0 ! video/x-raw, width=960, height=616, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink'
+GSTREAMER_PIPELINE = 'nvarguscamerasrc ! video/x-raw(memory:NVMM), width=3280, height=2464, format=(string)NV12, framerate=21/1 ! nvvidconv flip-method=0 ! video/x-raw, width=480, height=320, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink'
+#GSTREAMER_PIPELINE = 'nvarguscamerasrc ! video/x-raw(memory:NVMM), width=3280, height=2464, format=(string)NV12, framerate=21/1 ! nvvidconv flip-method=0 ! video/x-raw, width=960, height=616, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink'
 
 def faceDetect():
     # Obtain face detection Haar cascade XML files from OpenCV
