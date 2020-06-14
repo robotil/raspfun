4fc7e8a6d07d9c1604c64fb202a456a9a7cfda9a
vtal <vtal@vtal>
2020-06-14 17:36:28 +0300

    Add some complexity to the form

--- realfun/videostream.py
+++ realfun/videostream.py
@@ -5,11 +5,15 @@ sg.theme('BluePurple')	# Add a touch of color
 framerates_ch = ('5','10', '15', '20', '25', '30')
 # All the stuff inside your window.
 layout = [  [sg.Text('Some text on Row 1')],
-            [sg.Text('UDP or RTSP'), sg.InputText()],
-            [sg.Text('bitrate [bit/s]'), sg.InputText()],
+            [sg.Text('IP address of streamer or udp client:'), sg.InputText('172.23.40.63')],
+            [sg.Text('Port:'), sg.InputText('8554')],
+            [sg.Frame(layout=[
+            [sg.Radio('UDP    ', "RADIO1", default=False, size=(10,1)), sg.Radio('RTSP', "RADIO1",default=True)]], title='Protocol',title_color='red', relief=sg.RELIEF_SUNKEN, tooltip='Use these to set flags')],      
+
+            [sg.Text('bitrate [bit/s]'), sg.InputText('1000000')],
             [sg.Text('framerate [fps]'), sg.Listbox(framerates_ch, size=(15,len(framerates_ch)), key='-FRAMERATE-', enable_events=True)],
-            [sg.Text('IP address of streamer or udp client:'), sg.InputText()],
-            [sg.Text('TCP/UDP Port:'), sg.InputText()],
+            
+            
             [sg.Button('Ok'), sg.Button('Cancel')] ]
 
 # Create the Window
@@ -19,8 +23,20 @@ while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED or event == 'Cancel':	# if user closes window or clicks cancel
         break
-    print('UDP or RTSP', values[0])
-    print('bitrate', values[1])
+    if event == 'Ok':
+        ourpipeline = ""
+        if values[2] == True:
+            ourpipeline = "gst-launch-1.0 nvarguscamerasrc ! '\''video/x-raw(memory:NVMM), format=NV12, width=720, height=480, framerate=" #10/1'\'' ! nvv4l2h264enc insert-sps-pps=true bitrate=500000 ! h264parse ! rtph264pay pt=96 ! udpsink host=172.23.40.58 port=5000 sync=false -e"
+            #ourpipeline = "gst-launch-1.0 nvarguscamerasrc ! '\''video/x-raw(memory:NVMM), format=NV12, width=720, height=480, framerate=10/1'\'' ! nvv4l2h264enc insert-sps-pps=true bitrate=500000 ! h264parse ! rtph264pay pt=96 ! udpsink host=172.23.40.58 port=5000 sync=false -e"
+        else:
+            ourpipeline = 'test-launch "( nvarguscamerasrc ! video/x-raw(memory:NVMM), width=(int)720, height=(int)480, format=(string)NV12, framerate=(fraction)"' #10/1 ! nvv4l2h264enc insert-sps-pps=true bitrate=1000000 ! h264parse !   rtph264pay name=pay0 pt=96 )"'
+            #ourpipeline = 'test-launch "( nvarguscamerasrc ! video/x-raw(memory:NVMM), width=(int)720, height=(int)480, format=(string)NV12, framerate=(fraction)10/1 ! nvv4l2h264enc insert-sps-pps=true bitrate=1000000 ! h264parse !   rtph264pay name=pay0 pt=96 )"'
+        print(ourpipeline)
+    print('IP Address', values[0])
+    print('Port', values[1])
+    print('UDP', values[2])
+    print('RTSP', values[3])
+    print('Bitrate', values[4])
     print('framerate', values['-FRAMERATE-'][0])
 
 window.close()
