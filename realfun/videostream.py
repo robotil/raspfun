fffef310da83ceb98cc4eef4c24aed5b8a0576c1
vtal <vtal@vtal>
2020-06-15 08:12:28 +0300

    RTSP/UDP works

--- realfun/videostream.py
+++ realfun/videostream.py
@@ -6,10 +6,10 @@ sg.theme('BluePurple')	# Add a touch of color
 framerates_ch = ('5','10', '15', '20', '25', '30')
 # All the stuff inside your window.
 layout = [  [sg.Text('Some text on Row 1')],
-            [sg.Text('IP address of streamer or udp client:'), sg.InputText('172.23.40.63')],
-            [sg.Text('Port:'), sg.InputText('8554')],
+            [sg.Text('IP address of streamer or udp client:'), sg.InputText('172.23.40.58')],
+            [sg.Text('Port:'), sg.InputText('5000')],
             [sg.Frame(layout=[
-            [sg.Radio('UDP    ', "RADIO1", default=False, size=(10,1)), sg.Radio('RTSP', "RADIO1",default=True)]], title='Protocol',title_color='red', relief=sg.RELIEF_SUNKEN, tooltip='Use these to set flags')],      
+            [sg.Radio('UDP    ', "RADIO1", default=True, size=(10,1)), sg.Radio('RTSP', "RADIO1",default=False)]], title='Protocol',title_color='red', relief=sg.RELIEF_SUNKEN, tooltip='Use these to set flags')],      
 
             [sg.Text('bitrate [bit/s]'), sg.InputText('1000000')],
             [sg.Text('framerate [fps]'), sg.Listbox(framerates_ch, size=(15,len(framerates_ch)), key='-FRAMERATE-', enable_events=True)],
@@ -27,11 +27,14 @@ while True:
     if event == 'Ok':
         ourpipeline = ""
         if values[2] == True:
-            ourpipeline = "gst-launch-1.0 nvarguscamerasrc ! '\''video/x-raw(memory:NVMM), format=NV12, width=720, height=480, framerate=" #10/1'\'' ! nvv4l2h264enc insert-sps-pps=true bitrate=500000 ! h264parse ! rtph264pay pt=96 ! udpsink host=172.23.40.58 port=5000 sync=false -e"
+            ourpipeline = "gst-launch-1.0 nvarguscamerasrc ! \'video/x-raw(memory:NVMM), format=NV12, width=720, height=480, framerate=" #10/1'\'' ! nvv4l2h264enc insert-sps-pps=true bitrate=500000 ! h264parse ! rtph264pay pt=96 ! udpsink host=172.23.40.58 port=5000 sync=false -e"
+            ourpipeline = ourpipeline + values['-FRAMERATE-'][0]+"/1\' ! nvv4l2h264enc insert-sps-pps=true bitrate="+values[4]+ " ! h264parse ! rtph264pay pt=96 ! udpsink host=172.23.40.58 port=5000 sync=false -e"
+            #ourpipeline = "gst-launch-1.0 nvarguscamerasrc ! '\\''video/x-raw(memory:NVMM), format=NV12, width=720, height=480, framerate=" #10/1'\'' ! nvv4l2h264enc insert-sps-pps=true bitrate=500000 ! h264parse ! rtph264pay pt=96 ! udpsink host=172.23.40.58 port=5000 sync=false -e"
             #ourpipeline = "gst-launch-1.0 nvarguscamerasrc ! '\''video/x-raw(memory:NVMM), format=NV12, width=720, height=480, framerate=10/1'\'' ! nvv4l2h264enc insert-sps-pps=true bitrate=500000 ! h264parse ! rtph264pay pt=96 ! udpsink host=172.23.40.58 port=5000 sync=false -e"
         else:
             ourpipeline = 'test-launch "( nvarguscamerasrc ! video/x-raw(memory:NVMM), width=(int)720, height=(int)480, format=(string)NV12, framerate=(fraction)' #10/1 ! nvv4l2h264enc insert-sps-pps=true bitrate=1000000 ! h264parse !   rtph264pay name=pay0 pt=96 )"'
-            ourpipeline = ourpipeline+values['-FRAMERATE-'][0]+"/1\"" #'\'' ! nvv4l2h264enc insert-sps-pps=true bitrate="+values[4]+" ! h264parse !   rtph264pay name=pay0 pt=96 )"'
+            ourpipeline = ourpipeline+values['-FRAMERATE-'][0]+'/1 ! nvv4l2h264enc insert-sps-pps=true bitrate='+values[4]+' ! h264parse !   rtph264pay name=pay0 pt=96 )"'
+            #ourpipeline = ourpipeline+values['-FRAMERATE-'][0]+'/1 ! nvv4l2h264enc insert-sps-pps=true bitrate='+values[4]+' ! h264parse !   rtph264pay name=pay0 pt=96 )"'
             #ourpipeline = 'test-launch "( nvarguscamerasrc ! video/x-raw(memory:NVMM), width=(int)720, height=(int)480, format=(string)NV12, framerate=(fraction)10/1 ! nvv4l2h264enc insert-sps-pps=true bitrate=1000000 ! h264parse !   rtph264pay name=pay0 pt=96 )"'
         print(ourpipeline)
         os.system(ourpipeline)
