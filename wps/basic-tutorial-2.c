7a0171409a8c5a9c0b223b5f41f00d9e32457c15
michele@apollo <mhallakstamler@gmail.com>
2020-09-10 11:29:20 +0300

    Preparing WPS

--- wps/basic-tutorial-2.c
+++ wps/basic-tutorial-2.c
@@ -1,7 +1,7 @@
 #include <gst/gst.h>
 
 int main(int argc, char *argv[]) {
-  GstElement *pipeline, *source, *sink, *filter1, *filter2;
+  GstElement *pipeline, *source, *sink;
   GstBus *bus;
   GstMessage *msg;
   GstStateChangeReturn ret;
@@ -12,22 +12,18 @@ int main(int argc, char *argv[]) {
   /* Create the elements */
   source = gst_element_factory_make ("videotestsrc", "source");
   sink = gst_element_factory_make ("autovideosink", "sink");
-  filter1 = gst_element_factory_make("vertigotv", "filter1");
-  filter2 = gst_element_factory_make("videoconvert", "filter2");
-
 
   /* Create the empty pipeline */
   pipeline = gst_pipeline_new ("test-pipeline");
 
-  if (!pipeline || !source || !sink || !filter1 || !filter2) {
+  if (!pipeline || !source || !sink) {
     g_printerr ("Not all elements could be created.\n");
     return -1;
   }
 
   /* Build the pipeline */
-  gst_bin_add_many (GST_BIN (pipeline), source, filter1, filter2, sink, NULL);
-  if ((gst_element_link (source, filter1) != TRUE) || (gst_element_link (filter1, filter2) != TRUE)
-       || (gst_element_link(filter2, sink) != TRUE)) {
+  gst_bin_add_many (GST_BIN (pipeline), source, sink, NULL);
+  if (gst_element_link (source, sink) != TRUE) {
     g_printerr ("Elements could not be linked.\n");
     gst_object_unref (pipeline);
     return -1;
