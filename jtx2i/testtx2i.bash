f818309761dcda9ac45d28260690a303ea777b61
shushu@motek <shushu@motek>
2020-11-25 16:28:31 +0200

    add gpio scripts and canbus text

--- jtx2i/testtx2i.bash
+++ jtx2i/testtx2i.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Load CPU"
 cd ~/git/CPULoadGenerator
-./set_load_90_imp.bash 0.9  3600 &
+./set_load_90_imp.bash 0.9  3600
 echo "Load GPU"
 cd ~/cuda-samples/NVIDIA_CUDA-10.0_Samples/2_Graphics/Mandelbrot
 gnome-terminal --name="1julia" -- bash -c 'cd ~/cuda-samples/NVIDIA_CUDA-10.0_Samples/2_Graphics/Mandelbrot;./Mandelbrot mode=1; exit'
