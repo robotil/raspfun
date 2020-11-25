103979d419302a56c8bb37d1c6da1cec64366772
shushu@motek <shushu@motek>
2020-11-25 16:42:48 +0200

    Synchronization between scripts and raspfun

--- jtx2i/testtx2i.bash
+++ jtx2i/testtx2i.bash
@@ -1,4 +1,9 @@
 #!/bin/bash
+echo 1 | sudo -S nvpmodel -q #--verbose
+# echo 1 | sudo -S nvpmodel -m 0
+# echo 1 | sudo hwclock --systohc
+# echo 1 | sudo jetson_clocks
+
 echo "Load CPU"
 cd ~/git/CPULoadGenerator
 ./set_load_90_imp.bash 0.9  3600
@@ -9,5 +14,3 @@ sleep 1
 gnome-terminal --name="2julia" -- bash -c 'cd ~/cuda-samples/NVIDIA_CUDA-10.0_Samples/2_Graphics/Mandelbrot;./Mandelbrot mode=1; exit'
 sleep 1
 gnome-terminal --name="3julia" -- bash -c 'cd ~/cuda-samples/NVIDIA_CUDA-10.0_Samples/2_Graphics/Mandelbrot;./Mandelbrot mode=1; exit'
-
- 
